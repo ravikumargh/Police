7f2fc9afffd9542296104bad5b9bdb2b39cb4956
Ravikumar <ravikumar.gh@excelindia.com>
2017-05-11 13:32:09 +0530

    resizeing header nav on scroll

--- modules/core/client/controllers/home.client.controller.js
+++ modules/core/client/controllers/home.client.controller.js
@@ -6,7 +6,7 @@
     .controller('HomeController', HomeController);
   HomeController.$inject = ['$scope', 'ArticlesTopService'];
 
-  function HomeController($scope,ArticlesTopService) {
+  function HomeController($scope, ArticlesTopService) {
     var vm = this;
     vm.topArticles = ArticlesTopService.query();
     $(document).ready(function () {
@@ -31,6 +31,47 @@
         speed: 6000 // time the transition takes (ms)
       });
     });
+
+    // Hide Header on on scroll down
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = $('header').outerHeight();
+
+    $(window).scroll(function (event) {
+      didScroll = true;
+    });
+
+    setInterval(function () {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 250);
+
+    function hasScrolled() {
+      var st = $(window).scrollTop();
+
+      // Make sure they scroll more than delta
+      if (Math.abs(lastScrollTop - st) <= delta)
+        return;
+
+      // If they scrolled down and are past the navbar, add class .nav-up.
+      // This is necessary so you never see what is "behind" the navbar.
+      if (st > lastScrollTop && st > navbarHeight) {
+        // Scroll Down
+        $('#header-top').removeClass('nav-down').addClass('nav-up');
+        $('#header-nav').removeClass('nav-down').addClass('nav-up');
+      } else {
+        // Scroll Up
+        if (st + $(window).height() < $(document).height()) {
+          $('#header-top').removeClass('nav-up').addClass('nav-down');
+          $('#header-nav').removeClass('nav-up').addClass('nav-down');
+        }
+      }
+
+      lastScrollTop = st;
+    }
   }
 
 
